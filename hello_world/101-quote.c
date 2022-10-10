c232b44ffed22a7c5a009a8220c46b0c0b77a573
Angela <angelavasquez05@gmail.com>
2022-10-09 20:32:07 -0700

    Unix

--- hello_world/101-quote.c
+++ hello_world/101-quote.c
@@ -5,8 +5,8 @@
 #include <unistd.h>
 
 /**
- * main - Prints "and that piece of art is useful" 
- * - Dora Korpar, 2015-10-19
+ * main - Prints "and that piece of art is useful" -
+ * Dora Korpar, 2015-10-19
  *
  * Return: 1
 */
