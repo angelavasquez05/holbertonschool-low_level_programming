9e9214b7b794b8de0fd539b71cac761110102a1e
Angela <angelavasquez05@gmail.com>
2022-10-09 19:36:04 -0700

    Hello, puts

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 
-int main(void)
 /*
- *main "Programming is like building a multilingual puzzle", using puts
- *Return 0
- */
+main "Programming is like building a multilingual puzzle", using puts
+*Return 0
+*/
+int main(void)
+
 {
 puts("\"Programming is like building a multilingual puzzle");
 
