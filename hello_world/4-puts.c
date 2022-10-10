a68e8c23d82e9dececd4312ec75e8866d72fc867
Angela <angelavasquez05@gmail.com>
2022-10-09 19:24:12 -0700

    Hello, puts

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
-
+/*
+ *C program that prints exactly "Programming is like building a multilingual puzzle", followed by a new line.
+ *Use the function puts
+ *Your program should end with the value 0
+ */
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
