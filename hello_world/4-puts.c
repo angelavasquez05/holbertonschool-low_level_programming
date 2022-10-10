24b0bd707351ca2ec98791ba3cdfc042724fe1a3
Angela <angelavasquez05@gmail.com>
2022-10-09 19:26:37 -0700

    Hello, puts

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 /*
- *C program that prints exactly "Programming is like building a multilingual puzzle", followed by a new line.
- *Use the function puts
+ *prints exactly "Programming is like building a multilingual puzzle", followed by a new line, con puts
  *Your program should end with the value 0
  */
 int main(void)
