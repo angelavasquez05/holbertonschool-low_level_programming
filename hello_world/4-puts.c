1bd23a30d4f96231f32db470b56163d302ef1901
Angela <angelavasquez05@gmail.com>
2022-10-09 19:44:09 -0700

    Hello, puts

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,16 +1,6 @@
-/*
- *File: 4-puts.c
- *Author: Angela Maria Vasquez
- */
-
 #include <stdio.h>
 
-/*
- *main "Programming is like building a multilingual puzzle", using puts
- *Return 0
-*/
 int main(void)
-
 {
 puts("\"Programming is like building a multilingual puzzle");
 
