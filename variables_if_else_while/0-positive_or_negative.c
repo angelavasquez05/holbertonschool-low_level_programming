81915c93f7a9c8bd437258229220f76ae474b7aa
Angela <angelavasquez05@gmail.com>
2022-10-11 08:49:51 -0700

    Positive and negative

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -1,20 +1,24 @@
 #include <stdlib.h>
 #include <time.h>
-/* more headers goes there */
+/* 
+ * File: 0-positive_or_negative.c 
+*/
 
-/* betty style doc for function main goes there */
+/**
+ * main - "Program that assigns a random number
+ * and prints whether it is positive or negative"
+*/
 int main(void)
 {
   int n;
 
   srand(time(0));
   n = rand() - RAND_MAX / 2;
-  if (n > 0)
-    {
-      printf("Number is positive %d\n", n)
+  if (n > 0){
+    printf("%d is positive\n", n)
 	}
   else {
-    printf("Number is negativo", n)
+    printf("%d is negativo\n", n)
       }
   
   return (0);
