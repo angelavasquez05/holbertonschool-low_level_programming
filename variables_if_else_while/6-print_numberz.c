442e4c041e3b8fa854927be071211efff1ebc700
Angela <angelavasquez05@gmail.com>
2022-10-12 13:51:21 -0700

    Numberz

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/*
+ * File: 6-print_numberz.c
+*/
+/**
+ * main - "Prints all single digit numbers of
+ * base 10 starting from 0
+ * not use any variable of type char"
+ * Return: (0)
+*/
+int main(void)
+{
+int num;
+for (num = 0; num < 9; num++)
+{
+putchar((num % 10) + '0');
+}
+putchar('\n');
+return (0);
+}
