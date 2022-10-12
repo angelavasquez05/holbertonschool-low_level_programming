ec6ca6c975be476c8bab0e354d03ac26a16ceafa
Angela <angelavasquez05@gmail.com>
2022-10-12 15:16:05 -0700

    Inventing a combination

--- variables_if_else_while/100-print_comb3.c
+++ variables_if_else_while/100-print_comb3.c
@@ -12,17 +12,13 @@
 int main(void)
 {
 int i, j;
-for (i = 0; i < 10; i++)
+for (i = 0; i < 9; i++)
 {
-for (j = 1; j < 10; j++)
+for (j = i + 1; j < 10; j++)
 {
 putchar((i % 10) + '0');
 putchar((j % 10) + '0');
-if (i == 9 && j == 9)
-{
-continue;
-}
-if (i == 1 && j == 0)
+if (i == 8 && j == 9)
 {
 continue;
 }
