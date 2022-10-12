d8aae716c99d2dc33911c5533ee04d753568dd11
Angela <angelavasquez05@gmail.com>
2022-10-12 15:24:54 -0700

    The combination is business

---
+++ variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,35 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/*
+ * File: 100-print_comb3.c
+*/
+/**
+ * main - "Prints all possible different combinations
+ * of two digits"
+ * Return: (0)
+*/
+int main(void)
+{
+int i, j, k;
+for (i = 0; i < 8; i++)
+{
+for (j = i + 1; j < 9; j++)
+{
+for (k = j + 1; k < 10; k++)
+{
+putchar((i % 10) + '0');
+putchar((j % 10) + '0');
+putchar((k % 10) + '0');
+if (i == 8 && j == 9 && k == 10)
+{
+continue;
+}
+putchar(',');
+putchar(' ');
+}
+}
+}
+putchar('\n');
+return (0);
+}
