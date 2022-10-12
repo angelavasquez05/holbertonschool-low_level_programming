ea471770f97419c9ab25afbf693c26834e16e3d9
Angela <angelavasquez05@gmail.com>
2022-10-12 15:30:41 -0700

    The combination is business

--- variables_if_else_while/101-print_comb4.c
+++ variables_if_else_while/101-print_comb4.c
@@ -21,7 +21,7 @@ for (k = j + 1; k < 10; k++)
 putchar((i % 10) + '0');
 putchar((j % 10) + '0');
 putchar((k % 10) + '0');
-if (i == 8 && j == 9 && k == 10)
+if (i == 7 && j == 8 && k == 9)
 {
 continue;
 }
