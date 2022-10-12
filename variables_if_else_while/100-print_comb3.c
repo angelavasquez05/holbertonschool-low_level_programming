2d73a1412afde5a849fa9566a5ee664b8ef850c2
Angela <angelavasquez05@gmail.com>
2022-10-12 14:58:44 -0700

    Inventing a combination

---
+++ variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,31 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/*
+ * File: 100-print_comb3.c
+*/
+/**
+ * main - "Prints all possible different combinations
+ * of two digits"
+ * Return: (0)
+*/
+int main(void)
+{
+int i, j;
+for (i = 0; i < 10; i++)
+{
+for (j = 0; j < 10; j++)
+{
+putchar((i % 10) + '0');
+putchar((j % 10) + '0');
+if (i == 9 && j == 9)
+{
+continue;
+}
+putchar(',');
+putchar(' ');
+}
+}
+putchar('\n');
+return (0);
+}
