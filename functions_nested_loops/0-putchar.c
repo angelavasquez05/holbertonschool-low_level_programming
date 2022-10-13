0542a4c833d4b9c5e2ad3c1f07130099c9dd00e5
Angela <angelavasquez05@gmail.com>
2022-10-13 14:39:19 -0700

    _putchar

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/*
+ * File: 0-putchar.c
+*/
+/**
+ * main - "Program that prints _putchar"
+ *
+ * Return: (0)
+ */
+int main(void)
+{
+	putchar('_');
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
+	putchar('\n');
+
+	return (0);
+}
