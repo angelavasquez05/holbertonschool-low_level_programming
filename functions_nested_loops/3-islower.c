92b1b2eeece0a3c234ac46889c934cfc6f157c63
Angela <angelavasquez05@gmail.com>
2022-10-14 15:08:49 -0700

    Islower

--- functions_nested_loops/3-islower.c
+++ functions_nested_loops/3-islower.c
@@ -5,7 +5,7 @@
 /**
  * _islower - "Write a function that checks for
  * lowercase character"
- * @c: The character to be checked 
+ * @c: The character to be checked
  * Return: (0)
  */
 int _islower(int c)
