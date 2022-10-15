810d37d6eb3e20578390650afdcbc047a8008a57
Angela <angelavasquez05@gmail.com>
2022-10-14 20:30:13 -0700

    a + b

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/*
+ * File: 10-add.c
+*/
+/**
+ * add - "Write a function that adds two integers and
+ * returns the result"
+ * @x: Number1
+ * @y: Number2
+ * Return: (int)
+ */
+int add(int x, int y)
+{
+	return (x + y);
+}
