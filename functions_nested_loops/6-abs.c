fa4a11a29ba5fe8602eeaff4dbb23f24b97580e4
Angela <angelavasquez05@gmail.com>
2022-10-14 19:44:22 -0700

    Absolute value

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/*
+ * File: 6-abs.c
+*/
+/**
+ * _abs - "Write a function that computes the absolute
+ * value of an integer"
+ * @c: The character to be checked
+ * Return: (0)
+ */
+int _abs(int c)
+{
+	if (c < 0)
+	{
+		int abs_val;
+
+		abs_val = c * -1;
+
+		return (abs_val);
+	}
+	return (c);
+}
